ad938ded6860f6193a18036763822c2efb9432b1
olegatorlelik <lelik.belokonevvv@gmail.com>
2020-09-09 12:12:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const cardToDo = document.querySelector("#to-do");
+const btnAddTasck = document.querySelector("#add");
+const inputTitle = document.querySelector("#valueTasck");
+const inputDiscription = document.querySelector("#discription");
+const divInProgress = document.querySelector(".inProgres__wrapper");
+const divDone = document.querySelector(".done__wrapper");
+const state = { todo: [], inprogress: [], done: [], delet: [] };
+
+btnAddTasck.addEventListener("click", (event) => {
+  const todoElement = getToDoElem();
+  state.inprogress.push(todoElement);
+  inputTitle.value = "";
+  inputDiscription.value = "";
+  divInProgress.innerHTML = "";
+  state.inprogress.forEach((elem) => {
+    divInProgress.innerHTML += createElem(elem.title, elem.discription);
+  });
+
+  const btnDone = [...document.querySelectorAll(".btnClick")];
+  btnDone.forEach((elem) => {
+    if (elem.id === "done") {
+      elem.addEventListener("click", (event) => {
+        
+      });
+    }
+  });
+});
+
+const getToDoElem = () => {
+  const title = inputTitle.value;
+  const discription = inputDiscription.value;
+  return { title: title, discription: discription };
+};
+
+const createElem = (title, disc) => {
+  const div = document.createElement("div");
+  return (div.innerHTML = `<div id="rer" >${title} ${disc}</div>
+    <button class = 'btnClick' id = "done">&#10004</button>
+    <button class = 'btnClick' id = "re">&#9998</button>
+    <button class = 'btnClick' id = "delet">&#10007</button>`);
+};
